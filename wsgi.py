19e8b721b46f885a0a40115cc0177717246b3151
usman <muhammad.usma@techverx.com>
2022-04-26 15:56:57 +0500

    initial

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,3 @@
-"""
-WSGI config for gettingstarted project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
-"""
-
-from app import app as application
-app = application
+from app.main import app
+if __name__ == "__main__":
+  app.run()
